828597df9987b1adaaa732f8ccccbe4aacc7a0eb
yaoyangdi <dingyaoyang2019@gmail.com>
2022-09-24 15:06:50 +1000

    Refactor to have multiple folders in the repo

--- vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "tutorial.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
