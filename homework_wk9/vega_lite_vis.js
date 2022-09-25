0508362d810c8157fac05aee08370bdff9baff8d
yaoyangdi <dingyaoyang2019@gmail.com>
2022-09-25 10:26:25 +1000

    Grident color platte modified.

--- homework_wk9/vega_lite_vis.js
+++ homework_wk9/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "tutorial.json";
+var vg_1 = "vis.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
